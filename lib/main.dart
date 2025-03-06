7868d8a847166d1e430794ddaf04990ae429d382
Fazal Hamza Khan <theunknown.pak@gmail.com>
2025-03-06 11:24:42 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,39 @@ void main() {
   runApp(const MyApp());
 }
 
-
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+   int x = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Lottery App"),
+          backgroundColor: Colors.greenAccent,
+        ),
+        body: SafeArea(
+          child: Center(
+            child: Text(
+              x.toString(),
+              style: TextStyle(fontSize: 80),
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            x++ ;
+          },
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
   }
 }
