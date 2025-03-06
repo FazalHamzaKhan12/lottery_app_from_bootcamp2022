fb0f1c5381f1277ea15079edbc37cb95fd13ec44
Fazal Hamza Khan <theunknown.pak@gmail.com>
2025-03-06 11:33:05 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());  // Removed `const` here
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-   int x = 0;
+  int x = 0;
+
+  // Function to increment x
+  void incrementX() {
+    setState(() {
+      x++;  // Increment the value of x by 1
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Lottery App"),
+          title: const Text("Lottery App"),
           backgroundColor: Colors.greenAccent,
         ),
         body: SafeArea(
           child: Center(
             child: Text(
               x.toString(),
-              style: TextStyle(fontSize: 80),
+              style: const TextStyle(fontSize: 80),
             ),
           ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            x++ ;
-          },
+          onPressed: incrementX,  // Call the incrementX function
           child: const Icon(Icons.add),
         ),
       ),
