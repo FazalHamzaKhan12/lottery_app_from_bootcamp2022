e56e0507561251b3a8131be1ad28d6914301fbd1
Fazal Hamza Khan <theunknown.pak@gmail.com>
2025-03-08 11:27:44 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,20 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(MyApp());  // Removed `const` here
+  runApp(const MyApp()); // Removed `const` here
 }
 
 class MyApp extends StatefulWidget {
-  MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  Random random = Random();
   int x = 0;
-
-  // Function to increment x
-  void incrementX() {
-    setState(() {
-      x++;  // Increment the value of x by 1
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,17 +24,75 @@ class _MyAppState extends State<MyApp> {
           title: const Text("Lottery App"),
           backgroundColor: Colors.greenAccent,
         ),
-        body: SafeArea(
-          child: Center(
-            child: Text(
-              x.toString(),
-              style: const TextStyle(fontSize: 80),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Text(
+                "Lottery winning number is 5",
+                style: const TextStyle(fontSize: 24),
+              ),
             ),
-          ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 250,
+              width: 300,
+              decoration: BoxDecoration(
+                  color: Colors.grey.withOpacity(.3),
+                  borderRadius: BorderRadius.circular(10)),
+              child: Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: x == 5
+                    ? Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.done_all_outlined,
+                            color: Colors.green,
+                            size: 35,
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            'Congratulation you have won the lottery you number is $x',
+                            textAlign: TextAlign.center,
+                          )
+                        ],
+                      )
+                    : Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.error_outline,
+                            color: Colors.red,
+                            size: 35,
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            'Better luck next time you number is $x try again',
+                            textAlign: TextAlign.center,
+                          )
+                        ],
+                      ),
+              ),
+            )
+          ],
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: incrementX,  // Call the incrementX function
-          child: const Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+              x = random.nextInt(10);
+            });
+          },
+          child: const Icon(Icons.refresh),
         ),
       ),
     );
